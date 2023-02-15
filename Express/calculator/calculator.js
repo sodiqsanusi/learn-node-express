ba747a8afd454ec1f3a27e4294aa11b2d36ab613
Sodiq Sanusi <ademilddiq@gmail.com>
2023-02-15 05:10:47 +0100

    Atomic commits to track progress better and retrace steps quicker

--- Express/calculator/calculator.js
+++ Express/calculator/calculator.js
@@ -20,3 +20,16 @@ app.get('/', (req, res) => {
   //* This is used because you mostly can't use relative file paths on servers. You don't know how the file structure is
   //* You can then get the file by concatenating its path from the '__dirname'
 })
+
+//* When a website makes a post request to our server, we presently don't have a way of handling it
+//* which will bring up a "404" error clientside. To handle post requests from a website/application:
+
+app.post('/', (req, res) => {
+  res.send('<h2>Thanks for submitting</h2>')
+})
+
+//* For us to be able to access the data that was sent in the POST request, we will have
+//* to install a package called "Body Parser"
+
+//* This package works with Express in making data and properties from POST/PUT request
+//* accessible in an easy manner
