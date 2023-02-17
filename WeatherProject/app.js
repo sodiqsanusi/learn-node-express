f3c6cbe0976c8c90838230e10c9acb87faeea8fd
Sodiq Sanusi <ademilddiq@gmail.com>
2023-02-17 04:03:40 +0100

    Atomic commits to track progress better and retrace steps quicker

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
 
 
 app.get('/', (req, res) => {
+
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=798849c0a56202960547ceb2d05bac02';
+  https.get(url, (response) => {
+    console.log(response)
+  })
+
+
   res.send('<h1>Weather App</h1>')
 })
 
