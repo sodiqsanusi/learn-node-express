3e0d9778d4f3952e4877e92ee32c800aba931858
Sodiq Sanusi <ademilddiq@gmail.com>
2023-03-06 10:30:44 +0100

    Atomic commits to track progress better and retrace steps quicker

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 let app = express();
 
+//* This is necessary so that we have access to serving static files (e.g images) from our express server
+app.use(express.static('public'))
+//* The string used in the method above should be the name of the folder on the server where you want to save your static files.
+app.use(bodyParser.urlencoded({extended: true}))
 
 let port = 3000;
 app.listen(port, () => {
@@ -13,3 +18,55 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/signup.html`);
 })
+
+app.post('/', (req, res) => {
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const email = req.body.mail;
+  let data = {
+    members: [{
+      email_address: email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: fName,
+        LNAME: lName,
+      }
+    }]
+  }
+
+  let JSONData = JSON.stringify(data);
+  // This JSONData above is what we'll be sending to the mailChimp's API
+
+  const list_id = 'c292cd123a';
+  let url = `https://us14.api.mailchimp.com/3.0/lists/${list_id}/`
+  let options = {
+    method: 'POST',
+    auth: 'ade:eb6dadd9e896ae7265da30812a0d3d25-us14',
+  }
+
+  const testRequest = https.request(url, options, (response) => {
+
+    if(response.statusCode === 200){
+      res.send('Your details have been successfully saved');
+    }else{
+      res.send('There was a problem in saving your details, try again');
+    }
+
+    response.on('data', (data) => {
+      data = JSON.parse(data);
+      console.log(data)
+    })
+  })
+
+  testRequest.write(JSONData);
+  testRequest.end();
+
+})
+
+// API KEY
+//eb6dadd9e896ae7265da30812a0d3d25-us14
+// Audience ID
+// c292cd123a
+
+// API Route
+// "https://$API_SERVER.api.mailchimp.com/3.0/lists/$list_id/members"
