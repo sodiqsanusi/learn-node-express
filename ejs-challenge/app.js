d4b2da21073cdac8f81f36b7c60b6be9ae9d4790
Sodiq Sanusi <ademilddiq@gmail.com>
2023-03-13 14:53:41 +0100

    Done with the EJS Challenge!
    This was interesting to finish tbh. even though EJS might not be a relevant backend technology in Node & Express it was still fun to use

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,6 +15,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let posts = [];
+
+let convertToLinkParam = (textToBeConverted) => {
+  let test = _.lowerCase(textToBeConverted);
+  test = _.replace(test, /\W/g, '-');
+  return test;
+}
+
 
 
 app.get('/', (req, res) => {
@@ -53,9 +61,32 @@ app.post('/compose', (req, res) => {
   let post = {
     title: req.body.postTitle,
     content: req.body.postBody,
+    postLink: convertToLinkParam(req.body.postTitle),
   }
   posts.push(post);
   res.redirect('/')
+})
+
+app.get('/posts/:postID', (req, res) => {
+  let gottenId = req.params.postID;
+  gottenId = _.lowerCase(gottenId);
+  let foundPost = false;
+  // let postOptions = {
+  //   id: gottenId,
+  //   post: posts[parseInt(gottenId) - 1],
+  // }
+  // res.render('post', postOptions)
+  for(let post of posts){
+    if(_.lowerCase(post.title) == gottenId){
+      foundPost = true
+      res.render('post', {
+        title: post.title,
+        content: post.content,
+      })
+      break;
+    }
+  }
+  !foundPost && console.log("Didn't find a match")
 })
 
 
