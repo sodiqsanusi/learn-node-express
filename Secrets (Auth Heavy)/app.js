89405ac947a5b17eb263a0d10913cb07d03b8ade
Sodiq Sanusi <ademilddiq@gmail.com>
2023-04-07 01:57:56 +0100

    Atomic commits to track progress better and retrace steps quicker

--- Secrets (Auth Heavy)/app.js
+++ Secrets (Auth Heavy)/app.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require('md5');
+const bcrypt = require("bcrypt");
+
+let encryptionSaltRounds = 7;
 
 mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/secretsDB');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public')); 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -25,9 +29,9 @@ let userSchema = new mongoose.Schema({
   },
 })
 
-const encryptSecret = process.env.ENCRYPT_SECRET || "Thiswillbeourlittlesecret?";
+// const encryptSecret = process.env.ENCRYPT_SECRET || "Thiswillbeourlittlesecret?";
 
-userSchema.plugin(encrypt, {secret: encryptSecret, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, {secret: encryptSecret, encryptedFields: ['password']});
 
 let User = new mongoose.model('User', userSchema);
 
@@ -41,17 +45,23 @@ app.route('/register')
     res.render('register');
   })
   .post((req, res) => {
-    let newUser = new User({
-      email: req.body.username.toLowerCase(),
-      password: req.body.password,
-    })
 
-    newUser.save().then(() => {
-      console.log("A new user has been saved correctly")
-      res.render("secrets");
-    }).catch(err => {
-      console.log(err, "There was an error in creating a user with your details. Try again")
-    })
+    bcrypt.hash(req.body.password, encryptionSaltRounds, (err, hash) => {
+      if(err) res.send("There was a problem in creating your account, try again")
+
+      let newUser = new User({
+        email: req.body.username.toLowerCase(),
+        password: hash,
+      })
+      newUser.save().then(() => {
+        console.log("A new user has been saved correctly")
+        res.render("secrets");
+      }).catch(err => {
+        console.log(err, "There was an error in creating a user with your details. Try again")
+      })
+
+    });
+
   });
 
 app.route('/login')
@@ -63,12 +73,17 @@ app.route('/login')
     let password = req.body.password;
 
     User.findOne({email: name}).then((response) => {
-      if(!response) throw new Error("Didn't find a user with the given details")
-      if(response.password == password){
-        res.render('secrets')
-      }else{
-        throw new Error("Wrong password inputted for the user")
-      }
+      if(!response) throw new Error("Didn't find a user with the given details");
+
+      bcrypt.compare(password, response.password).then(result => {
+        if(result){
+          res.render('secrets')
+        }else{
+          throw new Error("Wrong password inputted for the user")
+        }
+      }).catch(err => {
+        res.send(err + " There was an error in logging you in. Try again")
+      })
     }).catch(err => {
       res.send(err + " There was an error in logging you in. Try again")
     })
@@ -77,8 +92,9 @@ app.route('/login')
 
 
 
+
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-})
+});
