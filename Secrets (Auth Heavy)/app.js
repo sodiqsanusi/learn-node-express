dbf6acac7e7908201ba8fc35e7050bcd3dba7482
Sodiq Sanusi <ademilddiq@gmail.com>
2023-04-09 02:15:27 +0100

    tbh, was confused for most part of this level
    Will have to research more on it and do some independent learning before been confident in using cookies and sessions in a backend application

--- Secrets (Auth Heavy)/app.js
+++ Secrets (Auth Heavy)/app.js
@@ -4,9 +4,12 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 // const encrypt = require("mongoose-encryption");
 // const md5 = require('md5');
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-let encryptionSaltRounds = 7;
+// let encryptionSaltRounds = 7;
 
 mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/secretsDB');
 
@@ -15,17 +18,25 @@ const app = express();
 app.use(express.static('public')); 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+  saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || "longasstextforasecret",
+  resave: false,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
 
 let userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    // required: true,
   },
   password: {
     type: String,
-    required: true,
+    // required: true,
   },
 })
 
@@ -33,7 +44,14 @@ let userSchema = new mongoose.Schema({
 
 // userSchema.plugin(encrypt, {secret: encryptSecret, encryptedFields: ['password']});
 
+userSchema.plugin(passportLocalMongoose);
+
 let User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get('/', (req, res) => {
@@ -45,23 +63,38 @@ app.route('/register')
     res.render('register');
   })
   .post((req, res) => {
+    let newUsername = req.body.username.toLowerCase();
+    console.log(req.body)
 
-    bcrypt.hash(req.body.password, encryptionSaltRounds).then(hash => {
-      let newUser = new User({
-        email: req.body.username.toLowerCase(),
-        password: hash,
-      })
-
-      newUser.save().then(() => {
-        console.log("A new user has been saved correctly")
-        res.render("secrets");
-      }).catch(err => {
-        console.log(err, "There was an error in creating a user with your details. Try again")
-      })
-
-    }).catch(err => {
-      console.log(err, "There was an error in creating a user with your details. Try again")
+    User.register({username: newUsername}, req.body.password, (err, user) => {
+      if(err){
+        console.log(err);
+        res.redirect('/register');
+      }else{
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets')
+        })
+      }
     })
+
+
+    //* Using Bcrypt:
+    // bcrypt.hash(req.body.password, encryptionSaltRounds).then(hash => {
+    //   let newUser = new User({
+    //     email: req.body.username.toLowerCase(),
+    //     password: hash,
+    //   })
+
+    //   newUser.save().then(() => {
+    //     console.log("A new user has been saved correctly")
+    //     res.render("secrets");
+    //   }).catch(err => {
+    //     console.log(err, "There was an error in creating a user with your details. Try again")
+    //   })
+
+    // }).catch(err => {
+    //   console.log(err, "There was an error in creating a user with your details. Try again")
+    // })
   });
 
 app.route('/login')
@@ -72,24 +105,54 @@ app.route('/login')
     let name = req.body.username.toLowerCase();
     let password = req.body.password;
 
-    User.findOne({email: name}).then((response) => {
-      if(!response) throw new Error("Didn't find a user with the given details");
-
-      bcrypt.compare(password, response.password).then(result => {
-        if(result){
-          res.render('secrets')
-        }else{
-          throw new Error("Wrong password inputted for the user")
-        }
-      }).catch(err => {
-        res.send(err + " There was an error in logging you in. Try again")
-      })
-    }).catch(err => {
-      res.send(err + " There was an error in logging you in. Try again")
+    let newUser = new User({
+      username: name,
+      password: password
     })
+
+    req.login(newUser, (err) => {
+      if(err){console.log(err)}
+      else{
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets')
+        })
+      }
+    })
+
+    //* Using Bcrypt:
+    // User.findOne({email: name}).then((response) => {
+    //   if(!response) throw new Error("Didn't find a user with the given details");
+
+    //   bcrypt.compare(password, response.password).then(result => {
+    //     if(result){
+    //       res.render('secrets')
+    //     }else{
+    //       throw new Error("Wrong password inputted for the user")
+    //     }
+    //   }).catch(err => {
+    //     res.send(err + " There was an error in logging you in. Try again")
+    //   })
+    // }).catch(err => {
+    //   res.send(err + " There was an error in logging you in. Try again")
+    // })
 
   });
 
+app.get('/secrets', (req, res) => {
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }else{
+    res.redirect('/login');
+  }
+})
+
+app.get('/logout', (req, res) => {
+  req.logOut((err) => {
+    if(!err){
+      res.redirect('/')
+    }
+  });
+})
 
 
 
